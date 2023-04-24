891837cfa05dcd791644b5607702d3717ac93208
GK-Amway <incgnkj8@amway.com>
2023-04-24 21:04:23 +0530

    HomePage, Sidebar, AddToCart Functionality - Day1

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
+import { BsEmojiWink } from 'react-icons/bs';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+
+export const CartContext = createContext();
+
+const CartProvider = ({children}) => {
+
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product, id) => {
+    const newItem = { ...product, quantity: 1 };
+    
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    })
+
+    if(cartItem) {
+      const newCart = [...cart].map((item) => {
+        if(item.id === id) {
+          return {...item, quantity: item.quantity + 1}
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    }
+    else {
+      setCart([...cart, newItem]);
+    }
+  }
+
+  return <CartContext.Provider value={{addToCart, cart}}>
+    {children}
+  </CartContext.Provider>
 };
 
-export default CartContext;
+export default CartProvider;
